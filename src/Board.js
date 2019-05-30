bc20b92c6e54aa67616d97931eee39a8f0fbb09b
Kritika Dusad <kritikadusad@gmail.com>
2019-05-30 11:08:53 -0700

    added grid container.

--- src/Board.js
+++ src/Board.js
@@ -19,10 +19,22 @@ class Board extends Component {
     createGrid() {
             let grid = [];
             // Making a 8x8 grid 
-            for (let i = 0; i < 9; i++){
+            for (let i = 0; i < 8; i++){
                 grid[i] = [];
-                for (let j = 0; j < 9; j++){
+                for (let j = 0; j < 8; j++){
                     const new_jewel = this.generateJewel();
+                    if (i > 1 && new_jewel === grid[i-1][j]) {
+                        console.log("found same adj cell.")
+                        if (new_jewel === grid[i-2][j]){
+                            const new_jewel = this.generateJewel();
+                        }
+                    }
+                    else if (j > 1 && new_jewel === grid[i][j-1]) {
+                        console.log("found same adj cell.")
+                        if (new_jewel === grid[i][j-2]) {
+                            const new_jewel = this.generateJewel();
+                        } 
+                    }
                     grid[i][j] = [new_jewel];
                 }
             }
@@ -32,9 +44,20 @@ class Board extends Component {
         this.setState( { game: [] })
     } 
   render() {
+    const boardGrid = this.createGrid()
+    let boardDiv = <div />
+    boardDiv = 
+        <div>
+            {boardGrid.map(jewel=>
+                <div className="grid-container">
+                    <div className="grid-item"> { jewel } </div>
+                </div>
+            )}
+        </div>;
+
     return (
         <div className="board">
-            { this.createGrid() }
+            { boardDiv }
             <br />
             <button className="reset-button" onClick = { this.resetGame.bind(this) }>Reset</button>    
         </div>
