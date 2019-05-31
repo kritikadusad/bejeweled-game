bc635eaaff537646ea5aaebff9e4415e6b862a93
Kritika Dusad <kritikadusad@gmail.com>
2019-05-31 01:33:33 -0700

    Cell component added to provide color specificity to grid cells

---
+++ src/Cell.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+//import './Cell.css';
+
+class Cell extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            game : []
+        }
+        //Bindings:
+    }
+    render() {
+        const colorDict = { 
+            1: "white",
+            2: "pink",
+            3: "blue",
+            4: "red",
+            5: "yellow",
+            6: "purple",
+            7: "aqua"
+        };
+                
+        return (
+            <div className = {colorDict[this.props.jewel]}>
+                ""
+            </div>
+            );
+    }
+}
+
+export default Cell;
