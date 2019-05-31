3ba38df18c69c10ef6ae97d4942146018fae4ea4
Kritika Dusad <kritikadusad@gmail.com>
2019-05-31 16:37:07 -0700

    logic for checking if at least one legal move possible.

--- src/Board.js
+++ src/Board.js
@@ -13,6 +13,9 @@ class Board extends Component {
         this.generateJewel = this.generateJewel.bind(this)
         this.createGrid = this.createGrid.bind(this)
         this.jewelRemove = this.jewelRemove.bind(this)
+        this.countSameAdjacent = this.countSameAdjacent.bind(this)
+        this.checkGrid = this.checkGrid.bind(this)
+        this.checkMovePossible = this.checkMovePossible.bind(this)
         this.checkAdjacentCells = this.checkAdjacentCells.bind(this)
     }
     generateJewel(arr) {
@@ -39,17 +42,17 @@ class Board extends Component {
                 let newJewel = this.generateJewel(arr);
                 if (i > 1) {
                     let sameCell = this.checkAdjacentCells(newJewel, grid[i-1][j] , grid[i-2][j]);
-                    console.log(newJewel, grid[i-1][j], grid[i-2][j],sameCell)
+                    //console.log(newJewel, grid[i-1][j], grid[i-2][j],sameCell)
                     if (sameCell) {
                         let filteredArr = this.jewelRemove(arr, newJewel)
-                        console.log(filteredArr)
+                       // console.log(filteredArr)
                         newJewel = this.generateJewel(filteredArr);
                         console.log(newJewel)
                     }
                 }
                 if (j > 1) {
                     let sameCell = this.checkAdjacentCells(newJewel, grid[i][j-1] , grid[i][j-1]);
-                    console.log(sameCell)
+                    //console.log(sameCell)
                     if (sameCell) {
                         let filteredArr = this.jewelRemove(arr, newJewel) 
                         newJewel = this.generateJewel(filteredArr);
@@ -64,9 +67,77 @@ class Board extends Component {
     resetGame () {
         this.setState( { game: [] })
     } 
+
+    countSameAdjacent( cell, i, j, grid){
+        let count = 0;
+        //Check cell above the given cell position
+        if ( i >= 1 && cell === grid[i+1][j] ){
+            count++;
+            }
+        //Check cell below the given cell position
+        if (i <= 6 && cell === grid[i-1][j] ){
+            count++;
+            }
+        //Check cell on the left of the given cell position
+        if (j>= 1 && cell === grid[i][j-1] ){
+            count++;
+            }
+        //Check cell on the right of the given cell position
+        if (j <= 6 && cell === grid[i][j-1] ){
+            count++;
+            }
+        return count
+    }
+    
+    
+    checkMovePossible(i,j, grid){
+        const cell = grid[i][j]
+        //Check if 2 cells above the given cell position
+        if (i >= 2 && cell === grid[i-1][j]) {
+           const count = this.countSameAdjacent(cell, i-2, j, grid)
+            if (count >= 2){
+                return true
+            }
+        }
+        //Check if 2 cells below the given cell position
+        if (i <= 5 && cell === grid[i+1][j]) {
+            const count = this.countSameAdjacent(cell, i+2, j, grid)
+            if (count >= 2){
+                return true
+            }
+        }
+        //Check if 2 cells left of the given cell position
+        if (j >= 2 && cell === grid[i][j-1]) {
+            if (this.countSameAdjacent(cell, i, j-2, grid) >= 2){
+                return true
+            }
+        }
+        //Check if 2 cells right of the given cell position
+        if (j <= 5 && cell === grid[i][j+1]) {
+            const count = this.countSameAdjacent(cell, i, j+2, grid)
+            if (count >= 2){
+                return true
+            }
+        }
+    }
+
+
+
+   //Check if grid has at least one possible move. If not, reset the game. 
+    checkGrid(grid){
+        for (let i = 0; i < 8; i++){
+            for (let j = 0; j < 8; j++){
+                if (this.checkMovePossible(i, j, grid)){
+                    return true
+                }
+            }
+        }
+        this.resetGame()
+    }    
     render() {
         const boardGrid = this.createGrid(jewels)
-        console.log(boardGrid)
+        //console.log(boardGrid)
+        this.checkGrid(boardGrid)
         let gridDiv = <div/>;
         let board = [];
         for ( let row of boardGrid ){
@@ -86,6 +157,7 @@ class Board extends Component {
 
         return (
             <div className = "board">
+                <h3> Lets play Bejeweled!</h3>
                 {gridDiv}
                 <button className="reset-button" onClick = { this.resetGame.bind(this) }>Reset</button>    
             </div>
